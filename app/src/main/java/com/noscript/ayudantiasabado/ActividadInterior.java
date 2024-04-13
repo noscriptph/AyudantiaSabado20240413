cd5900af0d2c9fa33966f1acf8919f6ae8d67340
victor <103396791+noscriptph@users.noreply.github.com>
2024-04-13 13:00:52 -0400

    archivos del ejercicio en clases

---
+++ app/src/main/java/com/noscript/ayudantiasabado/ActividadInterior.java
@@ -0,0 +1,19 @@
+package com.noscript.ayudantiasabado;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ActividadInterior extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_interior);
+
+    }
+}
